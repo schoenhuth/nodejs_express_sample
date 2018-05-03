9f60586554e5e810f25d6ba8e4626c2e2061919c
christian <christian@dev.com>
2018-05-03 14:39:00 +0200

    changed node js port

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(80, () => console.log('Example app listening on port 80!'))
