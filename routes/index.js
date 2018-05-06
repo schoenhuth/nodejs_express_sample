c8a8286a218516b13757a991c4744738f38ec0b0
christian <christian@dev.com>
2018-05-06 21:37:52 +0200

    hallo dani

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: 'hallo' });
+  res.render('index', { title: 'Express', message: 'hallo dani' });
 });
 
 module.exports = router;
