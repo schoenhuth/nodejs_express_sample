288443a99b27db55c74cd7d5580d5a8eee2ba4ed
christian <christian@dev.com>
2018-05-15 11:58:11 +0200

    test

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
-var indexRouter = require('./routes/index');
 var app = express()
-
-
 app.set('view engine', 'pug')
 
+//Routes
+var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
+
 app.use( '/', indexRouter);
+app.use( '/home', homeRouter);
 
 app.listen(8081, () => console.log('Example app listening on port 8081!'))
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 
-router.get('/home', function(req, res) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
