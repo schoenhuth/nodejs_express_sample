17f466a785f8166060a8a6b9117fcebd9ee4bb1a
christian <christian@dev.com>
2018-05-29 17:00:12 +0200

    added REST

--- routes/rest.js
+++ routes/rest.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET  */
-router.get('/', function(req, res, next) {
+router.get('/:paymentId/captures', function(req, res, next) {
 	//#1 create KP session
 	//#2 creat KP HPP
 
@@ -11,17 +11,15 @@ router.get('/', function(req, res, next) {
 
 	var response = {
 	  "responseCode": "Ok",
-	  "pspReferenceId": "",
+	  "pspReferenceId": req.params.paymentId,
 	  "timestamp": "2017-11-30T13:15:39.942Z",
 	  "paymentMethodBrandResponseCode": "string",
 	  "paymentMethodBrandResponseMessage": "string",
 	  "redirectionUrl": "string"
-	}
+	};
     
     res.send(JSON.stringify(response));
 
 });
-
-//reg.params.paymentId
 
 module.exports = router;
